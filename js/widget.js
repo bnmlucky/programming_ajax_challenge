bd4725c5568adf194fb0cc96d0316a345690c032
Natalia Titova <bnmlucky@gmail.com>
2020-01-23 13:39:05 -0500

    got a list of rooms to display correctly according to json data as html

--- js/widget.js
+++ js/widget.js
@@ -1,20 +1,20 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var rooms = JSON.parse(xhr.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for (var i = 0; i < rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
       } else {
-        statusHTML += '<li class="out">';
+        statusHTML += '<li class="full">';
       }
-      statusHTML += employees[i].name;
+      statusHTML += rooms[i].room;
       statusHTML += '</li>';
     }
     statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
+    document.getElementById('roomList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', '../data/employees.json');
+xhr.open('GET', '../data/rooms.json');
 xhr.send();
