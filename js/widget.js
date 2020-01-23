cf370d33b7d08d379d33d7517e182d4026415f0c
Natalia Titova <bnmlucky@gmail.com>
2020-01-23 13:50:40 -0500

    fixed bugs so that both room list and employee list show on the page according to data

--- js/widget.js
+++ js/widget.js
@@ -1,7 +1,7 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var rooms = JSON.parse(xhr.responseText);
+var xhr1 = new XMLHttpRequest();
+xhr1.onreadystatechange = function () {
+  if (xhr1.readyState === 4 && xhr1.status === 200) {
+    var rooms = JSON.parse(xhr1.responseText);
     var statusHTML = '<ul class="rooms">';
     for (var i = 0; i < rooms.length; i += 1) {
       if (rooms[i].available === true) {
@@ -16,5 +16,26 @@ xhr.onreadystatechange = function () {
     document.getElementById('roomList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', '../data/rooms.json');
+xhr1.open('GET', '../data/rooms.json');
+xhr1.send();
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    var employees = JSON.parse(xhr.responseText);
+    var statusHTML1 = '<ul class="bulleted">';
+    for (var i = 0; i < employees.length; i += 1) {
+      if (employees[i].inoffice === true) {
+        statusHTML1 += '<li class="in">';
+      } else {
+        statusHTML1 += '<li class="out">';
+      }
+      statusHTML1 += employees[i].name;
+      statusHTML1 += '</li>';
+    }
+    statusHTML1 += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML1;
+  }
+};
+xhr.open('GET', '../data/employees.json');
 xhr.send();
